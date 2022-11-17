ffe224f15cf14a9d6fe11c31ce529a86e0200344
blahablast <blahasam@gmail.com>
2022-11-17 09:34:23 -0500

    added Alert.js, AlertContext & AlertReducer

--- src/App.js
+++ src/App.js
@@ -4,24 +4,42 @@ import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
+import User from './pages/User'
+import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+    <GithubProvider>
+      <AlertProvider>
+        <BrowserRouter>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-        <main className='container mx-auto px-2 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+            <main className='container mx-auto px-2 pb-12'>
+              <Routes>
+                <Route
+                  path='/'
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
+                <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            </main>
+
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </AlertProvider>
+    </GithubProvider>
   )
 }
 
